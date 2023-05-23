1859dc0e5811e76f3c351d8142a3b1fb83d46dec
Pedro Tentugal <pedro.tentugal@amd.com>
2023-05-23 12:32:30 +0100

    First commit

---
+++ src/main/java/com/example/springtutorial/student/StudentService.java
@@ -0,0 +1,26 @@
+package com.example.springtutorial.student;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public List<Student> listStudents() {
+        return studentRepository.findAll();
+    }
+
+    public Optional<Student> listStudent(Long id) {
+        return studentRepository.findById(id);
+    }
+}
